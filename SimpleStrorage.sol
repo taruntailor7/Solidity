e18240e3519332e5eefdb529a2851925db85f6f1
Tarun Tailor <tarun@crimecheck.in>
2023-09-21 00:07:26 +0530

    First Function in Smart Contract

--- SimpleStrorage.sol
+++ SimpleStrorage.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.11;
+pragma solidity ^0.8.8;
 
 contract SimpleStorage {
-    uint256 FavouriteNumber;
+    // This gets initialized to Zero
+    // <- This means that this section is a comment!
+    // if we dont assign any value to this variable by default it will be assigned 0.
+
+    // uint256 favouriteNumber;  // Visibility is Private now for our Number.
+    uint256 public favouriteNumber; // Now Visibilit is Public for our Number.
+
+    function Store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
 }
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138 
--- SolidityTypes.sol
+++ SolidityTypes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.11;
+pragma solidity ^0.8.8;
 
 contract SimpleStorage {
     // Basic Solidity Types
