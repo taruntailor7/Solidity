b84a81f0a87a0abcdb2829fa950dc86d27a70fc0
Tarun Tailor <tarun@crimecheck.in>
2023-09-23 16:45:36 +0530

    Interacting with other contract & Will get or read the stored value from this contratc stored by this contract by calling Store funtion.

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -6,8 +6,24 @@ import "./SimpleStorage.sol";
 contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
 
+    // Creating another contract from this contract.
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage(); // Storing in simpleStorage variable
         simpleStorageArray.push(simpleStorage); // Pushing single simpleStorage into simpleStorageArray 
+    }
+
+    // Interacting with other contract.
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // For interacting with other contract we need below two things.
+        // Address
+        // ABI - Application Binary Interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.Store(_simpleStorageNumber);
+    }
+
+    // Will get or read the stored value from this contratc stored by this contract by calling Store funtion.
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
