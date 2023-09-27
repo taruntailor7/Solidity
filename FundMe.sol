d6bb6b31d11daa95c8cb41758a110a24c1c67ab9
Tarun Tailor <tarun@crimecheck.in>
2023-09-27 23:23:47 +0530

    Created a PriceConverter Library and moved Functions from FundMe.sol to Library and calling functions by importing that library

--- FundMe.sol
+++ FundMe.sol
@@ -5,9 +5,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
 
@@ -17,36 +18,11 @@ contract FundMe {
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
         // What is reverting?
         // undo any action before, and send remaininh gas back.
-    }
-
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        // 3000.00000000   
-        return uint256(price * 1e10); // 1**10 == 10000000000
-    }
-
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        // 3000_000000000000000000 = ETH / USD price
-        // 1_00000000000000000 ETH 
-
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        // 3000$
-        return ethAmountInUsd;
     }
 
     // function withraw() {}
