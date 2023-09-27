7896afc5b27602045a5c69f74bde0be9939193e8
Tarun Tailor <tarun@crimecheck.in>
2023-09-27 23:11:45 +0530

    Stored address of sender in funders array and addresses mapped to fund value

--- FundMe.sol
+++ FundMe.sol
@@ -11,11 +11,15 @@ contract FundMe {
 
     uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
 
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
         require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 = 1000000000000000000
-        // 18 decimals
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
         // What is reverting?
         // undo any action before, and send remaininh gas back.
     }
